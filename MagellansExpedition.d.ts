4b3fe7f3f3081f5762c29980af5dfb8c0d0f9bc7
dom111 <dom111@users.noreply.github.com>
2021-01-22 20:43:04 +0000

    Use published package version for `core-wonder`

--- MagellansExpedition.d.ts
+++ MagellansExpedition.d.ts
@@ -1,4 +1,3 @@
 import Wonder from '@civ-clone/core-wonder/Wonder';
-export declare class MagellansExpedition extends Wonder {
-}
+export declare class MagellansExpedition extends Wonder {}
 export default MagellansExpedition;
